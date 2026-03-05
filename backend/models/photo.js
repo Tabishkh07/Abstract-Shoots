c9875b72237a35de844680a96d89e9208cf15761
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-03-05 15:19:40 +0530

    feat: Image Schema

--- backend/models/photo.js
+++ backend/models/photo.js
@@ -1,1 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+// Image schema
+const ImageSchema = new Schema({
+    image:{
+        type:String, 
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+module.exports = mongoose.model('Image', ImageSchema);
